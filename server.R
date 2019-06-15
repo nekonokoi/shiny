b37a08d053a308ac73168c79ab47554ba8d8d638
nekonokoi <nekonokoi@nekonokoi.local>
2019-06-15 22:34:53 +0900

    add reactive

--- server.R
+++ server.R
@@ -5,32 +5,51 @@ res=summary(mdl)
 
 shinyServer(function(input,output){
 
-output$view <- reactiveTable(function() {
-head(iris[c(input$x,input$y)])
+# 事前に使うデータをつくる
+data <- reactive({
+  iris[,c(input$x,input$y)]
+})
+
+output$view <- renderTable(
+head(data())
+)
+
+# 事前に使うモデルを作る
+# reactiveとisolate
+res <- reactive({
+i1 <- iris
+x<-iris[,input$x]
+y<-iris[,input$y]
+
+mdl=lm(y~x)
+res=summary(mdl)
+return(res)
 })
   output$scatterPlot <- renderPlot(
     {
-      plot(iris$Sepal.Length,iris$Sepal.Width)
+      plot(data())
     }
   )
   #代入なので,はいらない
   output$string1=renderText(input$string1)
   output$distPlot<-renderPlot(
     {
-      hist(res$residuals)
+      hist(res()$residuals)
     }
   )
 
   output$regOutput=renderTable(
     {
-
-    res$coefficients
+    a <- res()
+    a$coefficients
     }
   )
 
   output$qqPlot<-renderPlot(
     {
-      qqplot(iris$Sepal.Length, iris$Sepal.Width)
+      x<-data()[,1]
+      y<-data()[,2]
+      qqplot(x,y)
     }
   )
 
