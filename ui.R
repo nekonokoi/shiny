eeecacf59c66949a943afd4553ab2c4b47524ed0
nekonokoi <nekonokoi@nekonokoi.local>
2019-06-16 14:47:27 +0900

    detail layout

--- ui.R
+++ ui.R
@@ -1,69 +1,83 @@
 library(shiny)
 
 shinyUI(
-  navbarPage("nav",
-    tabPanel("ページ1",
+  navbarPage("MyR",
+    tabPanel("アプリ1",
       mainPanel(
-      tabsetPanel(
-      tabPanel("aaa",h1("aaa")),
-      tabPanel("page2",sidebarLayout(
-
-
-          mainPanel = mainPanel(
-          tableOutput("choice_file"),
-          tableOutput("choice_table"),
-          plotOutput("multi.chart"),
-          tableOutput(
-            outputId = "view"
-          ),
-            plotOutput("scatterPlot"),
-            tableOutput(
-              outputId = "regOutput"
-            ),
-                  plotOutput("distPlot"),
-            plotOutput("qqPlot")
-          ),
-
-          sidebarPanel = sidebarPanel(
-          selectInput("select_file", label = "fileリスト", choices = ""),
-
-          selectInput("select_table", label = "テーブルリスト", choices = ""),
-            #引数としてつないでいく
-            textInput(
-              inputId = "string1",
-              label = "テキスト1を入力してください。",
-              value="",
-              placeholder="place",
-              width=NULL
+        tabsetPanel(
+          tabPanel(
+            "データ選択",
+            sidebarLayout(
+              sidebarPanel = sidebarPanel(
+                selectInput("select_file", label = "fileリスト", choices = "")
               ),
-              textAreaInput(
-                inputId = "string2",
-                label = "sqlとか",
-                value="",
-                placeholder="place",
-                width="300px",
-                height="300px",
-                cols=NULL,
-                rows=NULL,
-                resize=NULL
-                ),
-
-              selectInput(
-              inputId="x",
-              label="説明変数",
-              choices=c("Petal.Length","Petal.Width","Sepal.Length","Sepal.Width")
-              ),
-              selectInput(
-              inputId="y",
-              label="目的変数",
-              choices=c("Petal.Length","Petal.Width","Sepal.Length","Sepal.Width")
+              mainPanel=mainPanel(
+                h1('aaa'),
+                tableOutput("choice_file")
               )
+            )
+          ),
+          tabPanel("page2",
+            plotOutput("multi.chart")
+          ),
+
+          tabPanel("page3",
+            tabsetPanel(
+              tabPanel("回帰",
+                sidebarLayout(
+                  sidebarPanel = sidebarPanel(
+                    selectInput(
+                      inputId="x",
+                      label="説明変数",
+                      choices=c("Petal.Length","Petal.Width","Sepal.Length","Sepal.Width")
+                    ),
+                    selectInput(
+                      inputId="y",
+                      label="目的変数",
+                      choices=c("Petal.Length","Petal.Width","Sepal.Length","Sepal.Width")
+                    )
+                  ),
+                  mainPanel=mainPanel(
+                    plotOutput("scatterPlot"),
+                    tableOutput(outputId = "regOutput"),
+                    plotOutput("distPlot"),
+                    plotOutput("qqPlot")
+                  )
+                )
+              ),
+              tabPanel("残り",
+                sidebarLayout(
+                  mainPanel = mainPanel(
+                    tableOutput("choice_table"),
+                    tableOutput(outputId = "view")
+                  ),
+                  sidebarPanel = sidebarPanel(
+                    selectInput("select_table", label = "テーブルリスト", choices = ""),
+                    textInput(
+                      inputId = "string1",
+                      label = "テキスト1を入力してください。",
+                      value="",
+                      placeholder="place",
+                      width=NULL
+                    ),
+                    textAreaInput(
+                      inputId = "string2",
+                      label = "sqlとか",
+                      value="",
+                      placeholder="place",
+                      width="300px",
+                      height="300px",
+                      cols=NULL,
+                      rows=NULL,
+                      resize=NULL
+                    )
+                  )
+                )
+              )
+            )
           )
-      )
+        )#tabsetPanel
+      )#mainPanel
     )
-
-
+  )
 )
-)
-)
-))
