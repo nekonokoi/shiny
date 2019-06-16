18adee0e6708fe7cca8c7a1b236b2979985d367c
nekonokoi <nekonokoi@nekonokoi.local>
2019-06-16 14:30:46 +0900

    basic layout

--- ui.R
+++ ui.R
@@ -1,57 +1,69 @@
 library(shiny)
 
 shinyUI(
-  pageWithSidebar(
-    headerPanel = headerPanel(title="iris hist"),
-    mainPanel = mainPanel(
-    tableOutput("choice_file"),
-    tableOutput("choice_table"),
-    plotOutput("multi.chart"),
-    tableOutput(
-      outputId = "view"
-    ),
-      plotOutput("scatterPlot"),
-      tableOutput(
-        outputId = "regOutput"
-      ),
-            plotOutput("distPlot"),
-      plotOutput("qqPlot")
-    ),
+  navbarPage("nav",
+    tabPanel("ページ1",
+      mainPanel(
+      tabsetPanel(
+      tabPanel("aaa",h1("aaa")),
+      tabPanel("page2",sidebarLayout(
 
-    sidebarPanel = sidebarPanel(
-    selectInput("select_file", label = "fileリスト", choices = ""),
 
-    selectInput("select_table", label = "テーブルリスト", choices = ""),
-      #引数としてつないでいく
-      textInput(
-        inputId = "string1",
-        label = "テキスト1を入力してください。",
-        value="",
-        placeholder="place",
-        width=NULL
-        ),
-        textAreaInput(
-          inputId = "string2",
-          label = "sqlとか",
-          value="",
-          placeholder="place",
-          width="300px",
-          height="300px",
-          cols=NULL,
-          rows=NULL,
-          resize=NULL
+          mainPanel = mainPanel(
+          tableOutput("choice_file"),
+          tableOutput("choice_table"),
+          plotOutput("multi.chart"),
+          tableOutput(
+            outputId = "view"
+          ),
+            plotOutput("scatterPlot"),
+            tableOutput(
+              outputId = "regOutput"
+            ),
+                  plotOutput("distPlot"),
+            plotOutput("qqPlot")
           ),
 
-        selectInput(
-        inputId="x",
-        label="説明変数",
-        choices=c("Petal.Length","Petal.Width","Sepal.Length","Sepal.Width")
-        ),
-        selectInput(
-        inputId="y",
-        label="目的変数",
-        choices=c("Petal.Length","Petal.Width","Sepal.Length","Sepal.Width")
-        )
+          sidebarPanel = sidebarPanel(
+          selectInput("select_file", label = "fileリスト", choices = ""),
+
+          selectInput("select_table", label = "テーブルリスト", choices = ""),
+            #引数としてつないでいく
+            textInput(
+              inputId = "string1",
+              label = "テキスト1を入力してください。",
+              value="",
+              placeholder="place",
+              width=NULL
+              ),
+              textAreaInput(
+                inputId = "string2",
+                label = "sqlとか",
+                value="",
+                placeholder="place",
+                width="300px",
+                height="300px",
+                cols=NULL,
+                rows=NULL,
+                resize=NULL
+                ),
+
+              selectInput(
+              inputId="x",
+              label="説明変数",
+              choices=c("Petal.Length","Petal.Width","Sepal.Length","Sepal.Width")
+              ),
+              selectInput(
+              inputId="y",
+              label="目的変数",
+              choices=c("Petal.Length","Petal.Width","Sepal.Length","Sepal.Width")
+              )
+          )
+      )
     )
-  )
+
+
 )
+)
+)
+))
